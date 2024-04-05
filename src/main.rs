0d83f78e2ae6d682c1d731f6c128c61d760f2514
kriswind <126849955+kriswind@users.noreply.github.com>
2024-04-05 13:43:15 +0200

    winit rendering

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-fn main() {
-    let instances = wgpu::Instance::new(wgpu::InstanceDescriptor{
-        backends:wgpu::Backends::all(),
-        dx12_shader_compiler: Default::default(),
-    });
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
 
-    for adapter in instances.enumerate_adapters(wgpu::Backends::all()) {
-        println!("{:?}", adapter.get_info())
-    }
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = Window::new(&event_loop).unwrap();
+    window.set_title("my window");
+    env_logger::init();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        }
+    });
 }
