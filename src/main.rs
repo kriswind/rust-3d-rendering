f181ae80b9bf610ccdbd69bdf6a94cb94709006a
kriswind <126849955+kriswind@users.noreply.github.com>
2024-04-05 13:14:47 +0200

    thngs

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,8 @@ fn main() {
         backends:wgpu::Backends::all(),
         dx12_shader_compiler: Default::default(),
     });
+
+    for adapter in instances.enumerate_adapters(wgpu::Backends::all()) {
+        println!("{:?}", adapter.get_info())
+    }
 }
